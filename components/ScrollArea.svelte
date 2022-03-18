b5b33faa00f596d1aa460b78906dbc0c7c616b43
Miguel <jemiguelrivas@gmail.com>
2022-03-18 15:09:05 -0400

    adding scroll area

---
+++ components/ScrollArea.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { beforeUpdate } from "svelte";
+
+  export let horizontal = true,
+    vertical = true,
+    color = "transparent";
+
+  let computedHorizontal = !horizontal ? "no-horizontal" : "";
+  let computedVertical = !vertical ? "no-vertical" : "";
+
+  let computedClasses, computedLabel;
+
+  beforeUpdate(() => {
+    computedClasses = [
+      "scroll-area",
+      color,
+      computedHorizontal,
+      computedVertical,
+    ]
+      .join(" ")
+      .replace(/\s+/g, " ")
+      .trim();
+  });
+</script>
+
+<template>
+  <div on:scroll|passive class={computedClasses}>
+    <slot />
+  </div>
+</template>
