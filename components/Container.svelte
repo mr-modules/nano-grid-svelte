a4b5acec465adc6c9d9b567687953176750f0aa6
Miguel <jemiguelrivas@gmail.com>
2022-03-18 21:09:19 -0400

    adding container component

---
+++ components/Container.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { beforeUpdate } from "svelte";
+
+  export let className = "",
+    width = "";
+
+  let computedWidth, classes;
+  beforeUpdate(() => {
+    computedWidth = width !== "" ? `sz${width}` : "";
+    classes = ["container", className, computedWidth]
+      .join(" ")
+      .replace(/\s+/g, " ")
+      .trim();
+  });
+</script>
+
+<template>
+  <div class={classes}>
+    <slot />
+  </div>
+</template>
